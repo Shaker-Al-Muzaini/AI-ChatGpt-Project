a92de79ed1c1b9d20bdbe24cfed7655de665d49a
1shaker <salmazini@students.iugaza.edu.ps>
2023-09-10 22:32:12 +0300

    commit[3]

--- app/Http/Controllers/ChatGptStoreController.php
+++ app/Http/Controllers/ChatGptStoreController.php
@@ -3,21 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreChatRequest;
+use App\Models\Chat;
+use Illuminate\Support\Facades\Auth;
 use JetBrains\PhpStorm\NoReturn;
 use OpenAI\Laravel\Facades\OpenAI;
 
 class ChatGptStoreController extends Controller
 {
 
-    #[NoReturn] public function __invoke(StoreChatRequest $request){
-            $messages=[];
-            $messages[]=['role'=>'user','content',$request->input('promt')];
-            $response=OpenAI::chat()->create([
-                'model'=>'gpt-3.5-turbo',
-                'messages'=>$messages
-            ]);
-            $messages[]=['role'=>'assistant','content'=>$response->choices[0]->message->content];
-            dd($messages);
+    public function __invoke(StoreChatRequest $request, string $id = null)
+    {
+        $messages = [];
+        if ($id) {
+            $chat = Chat::findOrFail($id);
+            $messages = $chat->context;
+        }
+        $messages[] = ['role' => 'user', 'content' => $request->input('promt')];
+        $response = OpenAI::chat()->create([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => $messages
+        ]);
+        $messages[] = ['role' => 'assistant', 'content' => $response->choices[0]->message->content];
+        $chat = Chat::updateOrCreate(
+            [
+                'id' => $id,
+                'user_id' => Auth::id()
+            ],
+            [
+                'context' => $messages
+            ]
+        );
+
+        return redirect()->route('chat.show', [$chat->id]);
     }
 
 }
